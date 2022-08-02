2da69ea72d455dfd9d67a3955e960fbd6a2ee06a
m1212e <->
2022-08-02 13:52:03 +0200

    hopefully fixed tests

--- src/transpiler/generator/tests/recursive_class_generation.rs
+++ src/transpiler/generator/tests/recursive_class_generation.rs
@@ -35,7 +35,6 @@ mod tests {
         .unwrap();
 
         let h1 = hash_directory(&test_files.join("output")).unwrap();
-        println!("\n----------------\n");
         let h2 = hash_directory(&test_files.join("output_assert")).unwrap();
         assert_eq!(h1, h2);
 
@@ -50,7 +49,7 @@ mod tests {
     }
 
     /**
-        Simple compare helper. Not perfect, but enough for this case
+        Simple compare helper. Not perfect, but enough for this case because we got separate tests for the file content
     */
     fn hash_directory(dir: &std::path::Path) -> Result<u64, std::io::Error> {
         let mut paths: Vec<_> = std::fs::read_dir(dir)?.map(|r| r.unwrap()).collect();
@@ -59,21 +58,10 @@ mod tests {
         let mut hasher = DefaultHasher::new();
 
         for entry in paths {
-            println!("{}", entry.path().to_str().unwrap());
             if entry.file_type()?.is_dir() {
-                let hash = hash_directory(&entry.path())?;
-                print!(" hash: {}\n", hash);
-                hash.hash(&mut hasher);
+                hash_directory(&entry.path())?.hash(&mut hasher);
             } else {
-                let mut tmp = DefaultHasher::new();
-
-                let content = std::fs::read_to_string(&entry.path())?;
-                entry.file_name().to_str().unwrap().hash(&mut tmp);
-                content.hash(&mut tmp);
-
-                let hash = tmp.finish();
-                hash.hash(&mut hasher);
-                print!(" hash: {}\n", hash);
+                entry.file_name().to_str().unwrap().hash(&mut hasher);
             }
         }
 
