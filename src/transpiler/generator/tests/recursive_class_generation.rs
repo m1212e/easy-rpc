70b80c1ca5948ace467e0a031b8f5bfc8878b069
m1212e <->
2022-08-02 11:54:48 +0200

    File generation tested

--- src/transpiler/generator/tests/recursive_class_generation.rs
+++ src/transpiler/generator/tests/recursive_class_generation.rs
@@ -1,8 +1,14 @@
 #[cfg(test)]
 mod tests {
-    use std::path::Path;
+    use std::{
+        collections::hash_map::DefaultHasher,
+        hash::{Hash, Hasher},
+        path::Path,
+    };
 
-    use crate::transpiler::generator::{generate_for_directory_recursively, typescript::TypeScriptTranslator};
+    use crate::transpiler::generator::{
+        generate_for_directory_recursively, typescript::TypeScriptTranslator,
+    };
 
     #[test]
     fn test_success() {
@@ -18,10 +24,43 @@ mod tests {
             &test_files.join("input"),
             &test_files.join("output"),
             &"",
-            "Server"
-        ).unwrap();
+            "Server",
+        )
+        .unwrap();
 
-        //TODO: check results in dir and variable
+        assert_eq!(
+            hash_directory(&test_files.join("output")).unwrap(),
+            hash_directory(&test_files.join("output_assert")).unwrap()
+        );
+        
+        assert_eq!(result.get("Server").unwrap(), &vec!["api".to_string(), "auth".to_string()]);
+        assert_eq!(result.get("Client").unwrap(), &vec!["api".to_string(), "auth".to_string()]);
+    }
 
+    /**
+        Simple compare helper. Not perfect, but enough for this case
+    */
+    fn hash_directory(dir: &std::path::Path) -> Result<u64, std::io::Error> {
+        let paths = std::fs::read_dir(dir)?;
+        let mut hasher = DefaultHasher::new();
+
+        for entry in paths {
+            match entry {
+                Ok(entry) => {
+                    if entry.file_type()?.is_dir() {
+                        hash_directory(&dir.join(entry.file_name()))?.hash(&mut hasher);
+                    } else {
+                        let content = std::fs::read_to_string(&entry.path())?;
+                        entry.file_name().to_str().unwrap().hash(&mut hasher);
+                        content.hash(&mut hasher);
+                    }
+                }
+                Err(err) => {
+                    return Err(err);
+                }
+            }
+        }
+
+        Ok(hasher.finish())
     }
 }
---
+++ src/transpiler/generator/tests/recursive_class_generation_test_files/output_assert/Server/api/tracks.ts
@@ -0,0 +1,33 @@
+
+export default class tracks {
+    private server: any
+    /**
+        This method is used by easy-rpc internally and is not intended for manual use. It can be used to set the server of the object.
+    */
+    private setERPCServer(server: any) {
+        this.server = server
+
+        // trigger the setters to set the handlers on the server object
+        this.test6 = this.test6
+    }
+
+    constructor(callbacks?: {
+        test6: () => Promise<void>
+    }) {
+        if (callbacks?.test6) {
+            this.test6 = callbacks.test6
+        }
+
+    }
+
+    private _test6: () => Promise<void> = undefined as any
+    set test6(value: () => Promise<void>) {
+        this._test6 = value
+        this.server?.registerERPCCallbackFunction(value, "api\/tracks/test6")
+    }
+    get test6() {
+        return this._test6
+    }
+
+
+}
