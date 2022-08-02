829e3fc4d83054b5737e89ba762af8495142ed5b
m1212e <->
2022-08-02 12:25:19 +0200

    Fixed inconsistency in directory hashing for tests

--- src/transpiler/generator/tests/recursive_class_generation.rs
+++ src/transpiler/generator/tests/recursive_class_generation.rs
@@ -32,32 +32,33 @@ mod tests {
             hash_directory(&test_files.join("output")).unwrap(),
             hash_directory(&test_files.join("output_assert")).unwrap()
         );
-        
-        assert_eq!(result.get("Server").unwrap(), &vec!["api".to_string(), "auth".to_string()]);
-        assert_eq!(result.get("Client").unwrap(), &vec!["api".to_string(), "auth".to_string()]);
+
+        assert_eq!(
+            result.get("Server").unwrap(),
+            &vec!["api".to_string(), "auth".to_string()]
+        );
+        assert_eq!(
+            result.get("Client").unwrap(),
+            &vec!["api".to_string(), "auth".to_string()]
+        );
     }
 
     /**
         Simple compare helper. Not perfect, but enough for this case
     */
     fn hash_directory(dir: &std::path::Path) -> Result<u64, std::io::Error> {
-        let paths = std::fs::read_dir(dir)?;
+        let mut paths: Vec<_> = std::fs::read_dir(dir)?.map(|r| r.unwrap()).collect();
+
+        paths.sort_by_key(|a| a.path());
         let mut hasher = DefaultHasher::new();
 
         for entry in paths {
-            match entry {
-                Ok(entry) => {
-                    if entry.file_type()?.is_dir() {
-                        hash_directory(&dir.join(entry.file_name()))?.hash(&mut hasher);
-                    } else {
-                        let content = std::fs::read_to_string(&entry.path())?;
-                        entry.file_name().to_str().unwrap().hash(&mut hasher);
-                        content.hash(&mut hasher);
-                    }
-                }
-                Err(err) => {
-                    return Err(err);
-                }
+            if entry.file_type()?.is_dir() {
+                hash_directory(&dir.join(entry.file_name()))?.hash(&mut hasher);
+            } else {
+                let content = std::fs::read_to_string(&entry.path())?;
+                entry.file_name().to_str().unwrap().hash(&mut hasher);
+                content.hash(&mut hasher);
             }
         }
 
