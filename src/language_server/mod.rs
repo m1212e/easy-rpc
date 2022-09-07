b25b4a0458408d7f40f0ba8400bf405549e98e2d
m1212e <->
2022-09-07 03:44:46 +0200

    Basic working towerlsp setup

--- src/language_server/mod.rs
+++ src/language_server/mod.rs
@@ -1,97 +1,131 @@
-mod initialize;
-mod initialized;
-mod jsonrpc;
-mod show_message;
-mod shutdown;
-
-use std::{
-    collections::HashMap,
-    sync::{Arc, RwLock},
-    time::Duration,
-};
-
 use tokio::sync::mpsc;
 
 use crate::transpiler::ERPCError;
 
-use self::{
-    initialize::ServerCapabilities,
-    jsonrpc::{Error, JSONRPCServer},
-    show_message::MessageType,
-};
+use serde_json::Value;
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
 
-#[derive(Clone)]
-pub struct LanguageServer {
-    pub handlers: Arc<
-        RwLock<
-            HashMap<
-                String,
-                Box<
-                    dyn Fn(Option<serde_json::Value>) -> Result<serde_json::Value, Error>
-                        + Send
-                        + Sync,
-                >,
-            >,
-        >,
-    >,
-    pub server: JSONRPCServer,
+#[derive(Debug)]
+struct Backend {
+    client: Client,
 }
 
-impl LanguageServer {
-    pub fn new() -> LanguageServer {
-        LanguageServer {
-            handlers: Arc::new(RwLock::new(HashMap::new())),
-            server: JSONRPCServer::new(),
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult {
+            server_info: None,
+            capabilities: ServerCapabilities {
+                text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                    TextDocumentSyncKind::INCREMENTAL,
+                )),
+                completion_provider: Some(CompletionOptions {
+                    resolve_provider: Some(false),
+                    trigger_characters: Some(vec![".".to_string()]),
+                    work_done_progress_options: Default::default(),
+                    all_commit_characters: None,
+                    ..Default::default()
+                }),
+                execute_command_provider: Some(ExecuteCommandOptions {
+                    commands: vec!["dummy.do_something".to_string()],
+                    work_done_progress_options: Default::default(),
+                }),
+                workspace: Some(WorkspaceServerCapabilities {
+                    workspace_folders: Some(WorkspaceFoldersServerCapabilities {
+                        supported: Some(true),
+                        change_notifications: Some(OneOf::Left(true)),
+                    }),
+                    file_operations: None,
+                }),
+                ..ServerCapabilities::default()
+            },
+            ..Default::default()
+        })
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "initialized!")
+            .await;
+
+        self.client
+            .show_message(MessageType::INFO, "initialized lol")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+
+    async fn did_change_workspace_folders(&self, _: DidChangeWorkspaceFoldersParams) {
+        self.client
+            .log_message(MessageType::INFO, "workspace folders changed!")
+            .await;
+    }
+
+    async fn did_change_configuration(&self, _: DidChangeConfigurationParams) {
+        self.client
+            .log_message(MessageType::INFO, "configuration changed!")
+            .await;
+    }
+
+    async fn did_change_watched_files(&self, _: DidChangeWatchedFilesParams) {
+        self.client
+            .log_message(MessageType::INFO, "watched files have changed!")
+            .await;
+    }
+
+    async fn execute_command(&self, _: ExecuteCommandParams) -> Result<Option<Value>> {
+        self.client
+            .log_message(MessageType::INFO, "command executed!")
+            .await;
+
+        match self.client.apply_edit(WorkspaceEdit::default()).await {
+            Ok(res) if res.applied => self.client.log_message(MessageType::INFO, "applied").await,
+            Ok(_) => self.client.log_message(MessageType::INFO, "rejected").await,
+            Err(err) => self.client.log_message(MessageType::ERROR, err).await,
         }
+
+        Ok(None)
     }
 
-    pub async fn run(&mut self) -> Result<(), Error> {
-        self.server.run(self.handlers.clone()).await
+    async fn did_open(&self, _: DidOpenTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file opened!")
+            .await;
     }
 
-    fn register_handler<
-        F: Fn(Option<serde_json::Value>) -> Result<serde_json::Value, Error> + Send + Sync + 'static,
-    >(
-        &mut self,
-        method: String,
-        handler: F,
-    ) {
-        self.handlers
-            .write()
-            .unwrap()
-            .insert(method, Box::new(handler));
+    async fn did_change(&self, _: DidChangeTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file changed!")
+            .await;
+    }
+
+    async fn did_save(&self, _: DidSaveTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file saved!")
+            .await;
+    }
+
+    async fn did_close(&self, _: DidCloseTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file closed!")
+            .await;
+    }
+
+    async fn completion(&self, _: CompletionParams) -> Result<Option<CompletionResponse>> {
+        Ok(Some(CompletionResponse::Array(vec![
+            CompletionItem::new_simple("Hello".to_string(), "Some detail".to_string()),
+            CompletionItem::new_simple("Bye".to_string(), "More detail".to_string()),
+        ])))
     }
 }
 
 pub async fn start_language_server(mut rec: mpsc::Receiver<ERPCError>) {
-    let mut ls = LanguageServer::new();
+    let (stdin, stdout) = (tokio::io::stdin(), tokio::io::stdout());
 
-    ls.on_initialize(|_| {
-        Ok(initialize::Response {
-            capabilities: ServerCapabilities {},
-        })
-    });
-    ls.on_shutdown(|| Ok(()));
-    ls.on_initialized(|| {});
-
-    // tokio::spawn(async move {
-    //     loop {
-    //         match rec.recv().await {
-    //             Some(val) => {}
-    //             None => break,
-    //         };
-    //     }
-    // });
-
-    let ls2 = ls.clone();
-    tokio::spawn(async move {
-        tokio::time::sleep(Duration::from_secs(4)).await;
-
-        ls2.show_message(MessageType::Info, "This is a test info message".to_string())
-            .await
-            .unwrap()
-            .unwrap();
-    });
-
-    ls.run().await.unwrap();
+    let (service, socket) = LspService::new(|client| Backend { client });
+    Server::new(stdin, stdout, socket).serve(service).await;
 }
