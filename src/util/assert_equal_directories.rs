b5c0e16d4c44aaad1cc489378a80680fe0c62114
m1212e <->
2022-10-28 15:27:23 +0200

    Diff tool for tests

--- src/util/assert_equal_directories.rs
+++ src/util/assert_equal_directories.rs
@@ -1,10 +1,16 @@
 // only used in tests
+#[cfg(test)]
+use colored::Colorize;
+#[cfg(test)]
+use text_diff::{diff, Difference};
 
 /*
     Checks that every file existing in a also exists in b
 */
-#[allow(dead_code)]
+#[cfg(test)]
 pub fn assert_equal_directories(a: &std::path::Path, b: &std::path::Path) {
+    use colored::ColoredString;
+
     for entry in std::fs::read_dir(a).unwrap() {
         let entry = entry.unwrap();
         if entry.file_type().unwrap().is_dir() {
@@ -17,13 +23,38 @@ pub fn assert_equal_directories(a: &std::path::Path, b: &std::path::Path) {
                 )
             }
 
-            assert_eq!(
-                std::fs::read_to_string(&a.join(entry.file_name())).unwrap(),
-                std::fs::read_to_string(&b.join(entry.file_name())).unwrap(),
-                "\nComparison between\n{}\nand\n{}\nfailed.",
-                a.join(entry.file_name()).to_str().unwrap(),
-                b.join(entry.file_name()).to_str().unwrap()
+            let (_, changeset) = diff(
+                &std::fs::read_to_string(&a.join(entry.file_name())).unwrap(),
+                &std::fs::read_to_string(&b.join(entry.file_name())).unwrap(),
+                "\n",
             );
+            if changeset.len() > 0 {
+                let mut diff_counter = 0;
+
+                for diff in changeset {
+                    match diff {
+                        Difference::Same(ref x) => {
+                            println!("{}\n", x);
+                        }
+                        Difference::Add(ref x) => {
+                            print!("{}", format!("+{}\n", x).green());
+                            diff_counter += 1;
+                        }
+                        Difference::Rem(ref x) => {
+                            print!("{}", format!("-{}\n", x).red());
+                            diff_counter += 1;
+                        }
+                    }
+                }
+
+                if diff_counter > 0 {
+                    panic!(
+                        "\nComparison between\n{}\nand\n{}\nfailed.",
+                        a.join(entry.file_name()).to_str().unwrap(),
+                        b.join(entry.file_name()).to_str().unwrap()
+                    );
+                }
+            }
         }
     }
 }
